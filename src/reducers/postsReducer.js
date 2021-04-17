787e1617e2f56c5a8f47a9526136720e8ee5a726
Teinovic <momir.teinovic.2011@gmail.com>
2021-04-17 15:45:53 +0200

    hook postlist component attempt

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,7 +1,10 @@
-export default (state = [], action) => {
-    if (action.type === 'FETCH_POSTS') {
-        return action.payload
+export const postsReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'FETCH POSTS':
+            return action.payload
+        default:
+            return state
     }
 
-    return state
 }
+
